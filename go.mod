975a198bd1c61c07fad57623d936fb7f78e3ed5c
stefanoracke <stefanoracke@gmail.com>
2024-08-11 20:22:36 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/practice-structs
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"example.com/practice-structs/note"
+)
+
+func main() {
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+	if err != nil {
+
+		return
+	}
+	userNote.Display()
+
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title: ")
+
+	content := getUserInput("note content:")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Print(prompt)
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
